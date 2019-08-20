84fc9adcf31492cea354938c4536b61c650f045f
waferreira <wagner.ferreira@omnibees.com>
2019-08-20 09:23:13 -0300

    updates to deploy to heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  client_id: "23ec486d9f7bb0d43833",
+  client_secret: "58e9df23783851a308056c5f96de71b065f59981",
+  api_host: "https://cors-anywhere.herokuapp.com/https://github.com",
 };
